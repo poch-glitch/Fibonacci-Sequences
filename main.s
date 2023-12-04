71daade5af9779b0fccc5abe16a69453c222432b
poch-glitch <74361970+poch-glitch@users.noreply.github.com>
2023-12-04 02:00:13 -0500

    In the '.text' section, the 'main' label begins the program execution by adding an immediate value 4 to register , indicating a system call for printing a string.

--- main.s
+++ main.s
@@ -2,5 +2,9 @@
 	askUser : .asciiz "Find fibonacci of number (N>=25). Enter N: " 
  	invalid : .asciiz " We have an Illegal Number!"  
 	newline: .ascii "\n" 
+ 	 
+.text
+main:
+	addi $v0, $0, 4  
 
 
